11167990eb7b88d8770b77847369cafda1dced6f
Carlos Cantos <43449763+cantosc@users.noreply.github.com>
2018-10-16 16:11:24 +0200

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,7 +10,9 @@ while True:
         n = int(raw_input("Please enter a number: "))
         for x in range(n):
             x += 1
-            if (x % 3) == 0:
+            if (x % 3) == 0 and (x % 5) == 0:
+                print "fizzbuzz"
+            elif (x % 3) == 0:
                 print "fizz"
             elif (x % 5) == 0:
                 print "buzz"
